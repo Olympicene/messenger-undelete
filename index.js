201f51be327b6c89147361508bf1efec89ea06cd
Olympicene <kevinjcord@gmail.com>
2020-10-17 20:54:23 -0500

    soyjack finished

--- index.js
+++ index.js
@@ -1,33 +1,61 @@
 const fs = require("fs");
 var path = require('path');
 const login = require("facebook-chat-api");
+const request = require('request');
 
 login({appState: JSON.parse(fs.readFileSync('database/appstate.json', 'utf8'))}, (err, api) => {
     if(err) return console.error(err);
 
-    api.setOptions({listenEvents: true})
+    api.setOptions(
+        {
+        listenEvents: true,
+        selfListen: true
+    })
 
-    var timeout = 300000; // 1000 for one second
+    var timeout = 30000; // 1000 for one second
     var inTimeout = {};
+
+    function removeEmojis(string) {
+        var regex = /[^a-z0-9_]/gi;
+        var clean = string.replace(regex, '')
+        if(clean == '') {
+            clean = 'only alphanumeric characters because fuck you';
+        }
+        return clean;
+    }
+
+    var download = function(uri, filename, callback){
+        request.head(uri, function(err, res, body){
+            console.log('content-type:', res.headers['content-type']);
+            console.log('content-length:', res.headers['content-length']);
+
+            request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+    };
 
     api.listenMqtt((err, event) => {
         if(err) return console.error(err);
 
         var id = event.threadID;
-        console.log(event)
-        if(event.type == "message_reply" && !inTimeout[id]) {
+        if(event.type == "message_reply" && !inTimeout[id] && event.threadID == 2401681243197992) {
             switch(event.body) {
                 case "!soyjack":
-                    var msg = {
-                        url: "https://memegen.link/custom/" + event.messageReply.body.replace('/[^\w\s]/gi', '').split(' ').join('_') + ".jpg?alt=https://i.kym-cdn.com/photos/images/facebook/001/330/809/d90.png"
-                    }
-                    api.sendMessage(msg, event.threadID);
-                    if(timeout){
-                        inTimeout[id] = true;
-                        setTimeout(function(){
-                            inTimeout[id] = false;
-                        }, timeout);
-                    }
+                    var filter = removeEmojis(event.messageReply.body.split(' ').join('_'));
+                    console.log(filter);
+                    download("https://memegen.link/custom/" + filter + ".jpg?alt=https://i.kym-cdn.com/photos/images/facebook/001/330/809/d90.png", 'soyjack.png', function(){
+                        console.log('done');
+                        var msg = {
+                            //url: "https://memegen.link/custom/" + event.messageReply.body.replace('/[^\w\s]/gi', '').split(' ').join('_') + ".jpg?alt=https://i.kym-cdn.com/photos/images/facebook/001/330/809/d90.png"
+                            attachment: fs.createReadStream('soyjack.png')
+                        }
+                        api.sendMessage(msg, event.threadID);
+                        if(timeout){
+                            inTimeout[id] = true;
+                            setTimeout(function(){
+                                inTimeout[id] = false;
+                            }, timeout);
+                        }
+                      });
                 default:
             }
         } else if(inTimeout[id]){
