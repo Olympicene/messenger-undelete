3de174392d9f31f085565d05d9d3c6e8e5772b90
Olympicene <kevinjcord@gmail.com>
2021-07-02 17:57:47 -0500

    deleted anime to keep repo clean

--- database/config.js
+++ database/config.js
@@ -13,9 +13,9 @@ exports.allowed_threads = [
   "4258360417509656",
 ];
 
-exports.ignored_commands = ["ExampleCommand", "Undelete"];
+exports.ignored_commands = ["ExampleCommand"];
 
-exports.ignored_listeners = ['deleted', 'history',];
+exports.ignored_listeners = ["TEST"];
 
 exports.apiOptions = {
   listenEvents: true,
