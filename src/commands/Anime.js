ed3729ed56b15ed28f9f7e2d88b4b7c63ad86ae5
Olympicene <kevinjcord@gmail.com>
2021-07-07 08:33:34 +0000

    added description to anime command

--- src/commands/Anime.js
+++ src/commands/Anime.js
@@ -8,7 +8,7 @@ const fetch = require("node-fetch");
 module.exports = class ExampleCommand extends Command {
   constructor() {
     super();
-    this.description = " ";
+    this.description = " : reply to an screenshot to get source";
     this.type = ["message_reply"];
     this.message = {};
   }
