b88b46e24c635b66c5b400c01954640dd974e36b
Olympicene <kevinjcord@gmail.com>
2021-09-17 23:28:26 -0500

    added rage command

---
+++ src/commands/Rage.js
@@ -0,0 +1,34 @@
+const appRoot = require("app-root-path");
+const Command = require(appRoot + "/src/Command.js");
+const config = require(appRoot + "/database/config.js");
+var fs = require("fs");
+
+
+module.exports = class Rage extends Command {
+  constructor() {
+    super();
+    this.description = " ";
+    this.arguments = { _: 1 };
+    this.type = ["message", "message_reply"];
+    this.message = {};
+  }
+
+  async doAction(message, send, error, typingIndicator) {
+    //maybe temp but only way i can get the typing indicator
+    var stop = typingIndicator(message.threadID);
+    var id = setInterval(() => {typingIndicator(message.threadID)}, 5000);
+
+    
+    this.message.attachment = fs.createReadStream(appRoot + `/media/rage.jpg`);
+
+    send(this.message, message.threadID);
+
+    
+    //maybe temp but only way i can get the typing indicator
+    stop();
+    clearInterval(id);
+    return;
+  }
+
+  
+};
